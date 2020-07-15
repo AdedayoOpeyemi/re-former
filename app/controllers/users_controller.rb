8a85ed31b5f86c6ce18cb95a978ce4355c6ee98d
Osama Aldemeery <aldemeery@gmail.com>
2020-07-15 18:48:13 +0200

    Create new users.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,17 @@
 class UsersController < ApplicationController
-  def new; end
+  def new
+    @user = User.new
+  end
 
-  def create; end
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      redirect_to new_user_path
+    else
+      render :new
+    end
+  end
 
   private
 
